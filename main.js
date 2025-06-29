29e5d081de4f7f3ee0a45d838a40133ee3706ebf
Chanagun <chanagun_k@cmu.ac.th>
2025-06-30 02:11:38 +0700

    4.9 add the onSale

--- main.js
+++ main.js
@@ -6,11 +6,18 @@ createApp({
     const description = ref('cream boots boots')
     const image = ref('./assets/images/socks_green.jpg')
     const link = ref('https://www.camt.cmu.ac.th')
+    const inStock = ref(true)
+    const inventory = ref(100)
+    const onSale = ref(true)
+
     return {
       product, // ส่งออก product เพื่อให้ใช้ใน template ได้
       description,
       image,
-      link
+      link,
+      inStock,
+      inventory,
+      onSale
     }
   }
 }).mount('#app') // เชื่อม Vue กับ HTML ที่มี id="app"

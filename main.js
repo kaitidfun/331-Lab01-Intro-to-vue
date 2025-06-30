91c6843d66b8d6109c1804358fd8ae2fd80fc457
Chanagun <chanagun_k@cmu.ac.th>
2025-06-30 20:41:56 +0700

    8.5 add an onSale boolean

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
     setup() {
         const product = ref('Boots') // สร้างตัวแปร product ที่เก็บค่าแบบ reactive
         const description = ref('cream boots boots')
-        const image = ref('./assets/images/socks_green.jpg')
+        const brand = ref('SE 331')
         const link = ref('https://www.camt.cmu.ac.th')
-        const inStock = ref(false)
         const inventory = ref(100)
         const onSale = ref(true)
         const details = ref([
@@ -15,9 +14,25 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
         ])
+        const selectedVariant = ref(0) // เริ่มต้นเลือกตัวแรก
+        function updateVariant(index) {
+            selectedVariant.value = index
+        }
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity > 0
+        })
+        const onSaleText = computed(() => {
+            return onSale.value
+                ? `${brand.value} ${product.value} is on sale`
+                : `${brand.value} ${product.value} is not on sale`
+        })
+
         const sizes = ref([
             'S',
             'M',
@@ -27,6 +42,9 @@ createApp({
         function addToCart() {
             cart.value += 1
         }
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
         function updateImage(variantImage) {
             image.value = variantImage
         }
@@ -48,7 +66,11 @@ createApp({
             cart,
             addToCart,
             updateImage,
-            toggleInStock
+            toggleInStock,
+            brand,
+            title,
+            onSaleText,
+            updateVariant
         }
     }
 }).mount('#app') // เชื่อม Vue กับ HTML ที่มี id="app"
